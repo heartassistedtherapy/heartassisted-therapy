b41ce419fbb9eb0c649afb9c9fa854d86e20be68
DragonXexe <bramluyckx2008@gmail.com>
2026-05-26 20:55:53 +0200

    Moved to nkj file and seperated data to text.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,16 @@
+const markdownIt = require("markdown-it");
+const md = new markdownIt();
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("qr-code.png");
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addFilter("markdown", content => md.render(content));
+
+  return {
+      dir: {
+          input: ".",
+          data: "_data",
+      }
+  }
 };
---
+++ _data/text.js
@@ -0,0 +1,102 @@
+const title = "Heart-Assisted Therapy";
+const whatIsHat = `__Je bent hartelijk uitgenodigd voor de opleiding in de H.A.T. (Heart Assisted Therapy).__
+
+H.A.T. is een verrassend eenvoudige en effectieve methode in het veld van de psychotherapie. Het helpt emotionele verstoringen relatief snel en blijvend loslaten.
+
+Er wordt gebruik gemaakt van een combinatie van elementen, zoals 'awareness' (een stroom van associatieve ervaringen), de kracht van de taal, focusing op lichaamsgewaarwordingen, mindfullness en last but not least de energie van het hart. Doel is stabilisatie van emoties, cognities en lichaamssensaties, maar ook verwerking van psychotrauma , angsten, depressie e.d.. De vernieuwende rol van de hart-brein-connectie en van de lichaam/geest-verbinding speelde een sleutelrol in de ontwikkeling van de HAT.
+
+Dit model is een aanvulling op alle bestaande psycho-therapeutische oriëntaties. John Diepold, psycholoog bracht elementen samen uit de verschillende methoden, die hij in de loop van zijn 30-jarige ervaring geleerd heeft en die hij verwerkte tot een evenwichtig geheel.
+
+HAT is een zachte en gebruiksvriendelijke benadering, zowel voor therapeut als cliënt. Ze kan gemakkelijk geleerd worden door elke ervaren psychotherapeut.
+
+Deze methode is erop gericht om uw therapeutische luister-, observatie- en integratie-vaardigheden te activeren en maximaliseren. Ook worden er procedures gebruikt die de cliënt in staat stellen om te verwerken, patronen te veranderen en zichzelf verder te ontwikkelen.
+
+H.A.T. is geen therapie-richting op zich, maar leent zich als interventie tot een vlotte integratie in het elk therapeutisch werkkader dat door de behandelaar wordt gehanteerd.
+
+In de opleiding wordt theoretische uitleg afgewisseld met video's en veel praktische oefeningen om het protocol te leren gebruiken. Dit leidt tot een nieuwe vaardigheid, een tool als psychotherapeut.
+
+We organiseren deze opleiding samen met enkele collega's-psychologen.
+
+Collegiale groeten,
+
+Walter De Jongh, klinisch psycholoog, Kalmthout
+`;
+
+const originTitle = "Hoe het begon : oorsprong van de H.A.T.";
+
+const originText = `Ik leerde __John Diepold__ kennen in 2005 tijdens een
+seminarie in Utrecht. Wat mij opviel, was zijn originele manier om aan
+psychotherapie te doen. Fascinerend vond ik vooral zijn manier van
+kijken naar psychologische evolutie. Hoewel John heel wat theorieën uit
+de psychologie in zijn achterhoofd heeft, behoudt hij de
+nieuwsgierigheid van een kind. Het is zoals een kunstenaar, die naar de
+wereld kijkt vanuit een onbevangen verwondering.
+
+Deze bijna kinderlijke nieuwsgierigheid gaf hem de inspiratie om zijn nieuwe methode te ontwikkelen. Hij heeft een echte ontdekking gedaan: de energie van het hart hanteren in het therapeutisch proces. Bovendien integreerde hij als psycholoog zijn rijke kennis van 30 jaar uit meerdere psychotherapeutische methoden om een uitgebalanceerde werkwijze te ontwikkelen.`;
+
+const fundamenten = `De ontdekking van de hart-brein-connectie was dè sleutel tot de
+ontwikkeling van de H.A.T. John Diepold werkte eerst jarenlang met
+'klassieke' psychotherapeutische methoden : cognitieve therapie,
+experiëntieel (Carl Rogers), Gestalttherapie, Focusing (Gendlin), en met
+de hypnose van Milton Erickson, mindfullness, EMDR en ook TFT (Thought
+Field Therapy).
+            
+__Maar…__ toen hij het __hart__ betrok in het
+psychotherapeutisch proces, ontstond er opeens een nieuwe dynamiek bij
+zijn cliënten. Hij ondervond dat, toen hij de handen kruiselings op het
+hart liet leggen, het proces van de cliënt kon verdiepen en bevorderen.
+Dit betekende een doorbraak en tegelijk een paradigma-shift.
+`;
+const reden1 = "Snel aan te leren, in twee dagen";
+const reden2 =
+    "Toepasselijk voor de meeste emotionele en psychische problemen.";
+const reden3 =
+    "Reduceert gelijktijdig: storende intense emoties, piekergedachten en fysische gewaarwordingen.";
+const reden4 =
+    "Krachtig en efficiënt voor psychotrauma, depressie en angsten en nuttig bij verslaving en OCD.";
+const reden5 =
+    "Weinig intense arousal tijdens de sessie, wat vooral nuttig is bij psychotrauma.";
+const reden6 = "Tijdsbesparend en toch diepgaand.";
+const reden7 = "Combineerbaar met elke psychotherapeutische benadering.";
+const reden8 =
+    "Leidt tot meer emotioneel evenwicht, innerlijke rust en verbeterd zelfvertrouwen.";
+const reden9 = "Blijvend resultaat.";
+const reden10 = "Aangenaam om als psychotherapeut mee te werken.";
+
+const doelstelling1 = "Oorsprong en theoretische basis van de H.A.T. kennen.";
+const doelstelling2 = "Het basisprotocol kennen.";
+const doelstelling3 =
+    "De H.A.T. tijdens het practicum kunnen toepassen als therapeut.";
+const doelstelling4 =
+    "De H.A.T. in de rol van cliënt ervaren tijdens het practicum.";
+const doelstelling5 = "Observaties en ervaringen bespreken.";
+const doelstelling6 =
+    "De H.A.T. kunnen toepassen in de eigen praktijk als psychotherapeut.";
+
+module.exports = {
+    title,
+
+    whatIsHat,
+    originTitle,
+    originText,
+
+    fundamenten,
+
+    reden1,
+    reden2,
+    reden3,
+    reden4,
+    reden5,
+    reden6,
+    reden7,
+    reden8,
+    reden9,
+    reden10,
+
+    doelstelling1,
+    doelstelling2,
+    doelstelling3,
+    doelstelling4,
+    doelstelling5,
+    doelstelling6,
+};
