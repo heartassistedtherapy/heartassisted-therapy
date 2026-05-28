84c686c8b7880c0c3cb2d8143ca30ef47b59f015
DragonXexe <bramluyckx2008@gmail.com>
2026-05-28 18:46:01 +0200

    Fixed issues with loading content correctly

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,28 @@
 const markdownIt = require("markdown-it");
-const md = new markdownIt();
+const md = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true,
+});
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("qr-code.png");
-  eleventyConfig.addPassthroughCopy("admin");
-  eleventyConfig.addFilter("markdown", content => md.render(content));
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("qr-code.png");
+    eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addFilter("markdown", (content) => md.render(content));
+    eleventyConfig.addFilter("markdown", (content) => {
+        if (typeof content !== "string") {
+            console.log("Markdown filter received:", content);
+            return "";
+        }
 
-  return {
-      dir: {
-          input: ".",
-          data: "_data",
-      }
-  }
+        return md.render(content);
+    });
+
+    return {
+        dir: {
+            input: ".",
+            data: "_data",
+        },
+    };
 };
